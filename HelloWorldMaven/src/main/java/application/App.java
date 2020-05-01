2ad22790298cfab09f9db66a653e8796a521e123
John Purcell <john.purcell@caveofprogramming.com>
2020-05-01 11:39:30 +0200

    Created

---
+++ HelloWorldMaven/src/main/java/application/App.java
@@ -0,0 +1,11 @@
+package application;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ HelloWorldMavenEclipse/src/main/java/com/caveofprogramming/maven/HelloWorldMavenEclipse/App.java
@@ -0,0 +1,13 @@
+package com.caveofprogramming.maven.HelloWorldMavenEclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
