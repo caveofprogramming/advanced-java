3ab5483ae81befa7470d9949e31510f8b132a65c
John Purcell <john.purcell@caveofprogramming.com>
2020-05-11 16:02:58 +0200

    Created

---
+++ 05.090 Saving Users/src/main/java/com/caveofprogramming/db/UserDaoImpl.java
@@ -0,0 +1,38 @@
+package com.caveofprogramming.db;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserDaoImpl implements UserDao {
+
+	@Override
+	public void save(User t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Optional<User> findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(User t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(User t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<User> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
