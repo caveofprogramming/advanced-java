646b5bf0076de1a412944c18a6dec5a2ed299a88
John Purcell <john.purcell@caveofprogramming.com>
2020-05-22 19:33:01 +0200

    Created

--- 07.100 Retrieving Users in a Range/src/test/java/com/caveofprogramming/db/UserDaoImplTest.java
+++ 07.100 Retrieving Users in a Range/src/test/java/com/caveofprogramming/db/UserDaoImplTest.java
@@ -8,6 +8,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,7 +22,7 @@ public class UserDaoImplTest {
 	private Connection conn;
 	private List<User> users;
 
-	private static final int NUM_TEST_USERS = 1000;
+	private static final int NUM_TEST_USERS = 2;
 
 	private List<User> loadUsers() throws IOException {
 
@@ -73,6 +74,31 @@ public class UserDaoImplTest {
 		stmt.close();
 		
 		return id;
+	}
+	
+	private List<User> getUsersInRange(int minId, int maxId) throws SQLException {
+		
+		List<User> retrieved = new ArrayList<User>();
+		
+		var stmt = conn.prepareStatement("select id, name from user where minId >= ? and maxId <= ?");
+		
+		stmt.setInt(1, minId);
+		stmt.setInt(2, maxId);
+		
+		var rs = stmt.executeQuery();
+		
+		while(rs.next()) {
+			int id = rs.getInt("id");
+			String name = rs.getString("name");
+			
+			var user = new User(id, name);
+			
+			retrieved.add(user);
+		}
+		
+		stmt.close();
+		
+		return retrieved;
 	}
 	
 	@Test
