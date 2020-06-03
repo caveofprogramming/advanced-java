1c607723509e60cdafe19faaf3a7b42b2f689b71
John Purcell <john.purcell@caveofprogramming.com>
2020-06-03 12:10:24 +0200

    Created

---
+++ 09.050 Getting Atributes Values/src/app/Repository.java
@@ -0,0 +1,35 @@
+package app;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Repository<T> {
+    public void save(T t) {
+            
+            var clazz = t.getClass();
+            
+            var fields = clazz.getDeclaredFields();
+            
+            for(var field: fields) {
+                    var annotations = field.getAnnotationsByType(Field.class);
+                    
+                    if(annotations.length > 0) {
+                            System.out.println(field);
+                    }
+            }
+            
+            // @formatter:off
+            
+            var fieldList = Arrays
+                    .stream(clazz.getDeclaredFields())
+                    .filter(f -> f.getAnnotationsByType(Field.class).length > 0)
+                    .collect(Collectors.toList());
+            
+            // @formatter:on
+            
+            System.out.println();
+            
+            System.out.println(fieldList);
+    }
+}
+
