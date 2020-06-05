8f0ba7d09cae00c989233e859c0c42318817a036
John Purcell <john.purcell@caveofprogramming.com>
2020-06-05 14:23:36 +0200

    Created

---
+++ 10.050 Adding a Menu/src/main/java/application/App.java
@@ -0,0 +1,13 @@
+package application;
+
+import javax.swing.SwingUtilities;
+
+import controller.Controller;
+
+public class App {
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(Controller::new);
+	}
+
+}
