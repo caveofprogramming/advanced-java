65a2484005e43a5968dd5c57c1439d2191b4a2b0
John Purcell <john.purcell@caveofprogramming.com>
2020-06-05 14:16:00 +0200

    Created

---
+++ 10.040 Adding a Panel/src/main/java/gui/MainFrame.java
@@ -0,0 +1,15 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	public MainFrame() {
+		super("Swing Demo");
+		
+		setSize(1000, 800);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+}
