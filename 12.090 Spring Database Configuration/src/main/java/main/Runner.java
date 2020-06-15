043799b8fade06006eba80f71435172cff36c123
John Purcell <john.purcell@caveofprogramming.com>
2020-06-15 14:14:17 +0200

    Created

---
+++ 12.090 Spring Database Configuration/src/main/java/main/Runner.java
@@ -0,0 +1,21 @@
+package main;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import greeters.Greeter;
+
+@Component
+public class Runner implements CommandLineRunner {
+	
+	@Autowired
+	private Greeter greeter;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		greeter.greet();
+		
+	}
+}
