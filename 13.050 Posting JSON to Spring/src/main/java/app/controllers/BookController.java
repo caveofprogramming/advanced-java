5fe94b9fd3006eff5f2661a3ad77a24fe9f3cad9
John Purcell <john.purcell@caveofprogramming.com>
2020-06-18 14:58:41 +0200

    Created

--- 13.050 Posting JSON to Spring/src/main/java/app/controllers/BookController.java
+++ 13.050 Posting JSON to Spring/src/main/java/app/controllers/BookController.java
@@ -1,18 +1,29 @@
 package app.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.entities.Book;
 
 @RestController
-public class BookControllers {
+public class BookController {
 
 	@GetMapping("/books")
 	public Book books() {
-		
+
 		var book = new Book("Charles Dickens", "Great Expectations");
-		
+
 		return book;
 	}
+	
+	@PostMapping("/books")
+	public Book createBook(@RequestBody Book book) {
+		
+		System.out.println("Creating: " + book);
+		book.setId(7L);
+		return book;
+	}
+
 }
