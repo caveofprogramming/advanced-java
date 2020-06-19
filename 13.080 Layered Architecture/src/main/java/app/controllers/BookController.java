3f6f30692dd5b449c6fd82956dfb78e74b4ef198
John Purcell <john.purcell@caveofprogramming.com>
2020-06-19 10:59:17 +0200

    Created

---
+++ 13.080 Layered Architecture/src/main/java/app/controllers/BookController.java
@@ -0,0 +1,41 @@
+package app.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import app.entities.Book;
+
+@RestController
+public class BookController {
+
+	@GetMapping("/books")
+	public Book books() {
+
+		var book = new Book("Charles Dickens", "Great Expectations");
+
+		return book;
+	}
+	
+	@PostMapping("/books")
+	public Book createBook(@RequestBody Book book) {
+		
+		System.out.println("Creating: " + book);
+		book.setId(7L);
+		return book;
+	}
+	
+	@GetMapping("/books/{id}")
+	public Book getBook(@PathVariable("id") Long id) {
+		var book = new Book("Charles Dickens", "Great Expectations");
+		
+		System.out.println("Book ID: " + id);
+		
+		book.setId(id);
+
+		return book;
+	}
+
+}
